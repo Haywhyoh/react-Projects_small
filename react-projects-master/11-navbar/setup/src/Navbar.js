895105f338ab046af979291e1193962b850ef964
Adedayo Samuel Ayomide <adefeyisayo998@gmail.com>
2021-12-05 17:53:56 +0100

    Update Navbar.js

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -28,6 +28,28 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const {id, url, text} = link;
+              return(
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((socialIcon) =>{
+            const{id, url, icon} = socialIcon;
+            return(
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   )
